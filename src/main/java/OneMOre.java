9d532552f51371e03ecc73fad712e80e64135525
Ramzes <ganster200723@gmail.com>
2022-09-22 16:58:47 +0300

    1.0

---
+++ src/main/java/OneMOre.java
@@ -0,0 +1,13 @@
+import java.io.PrintStream;
+import java.net.CacheRequest;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class OneMOre {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+        System.out.println("Redactoring");
+    }
+}
+
+
